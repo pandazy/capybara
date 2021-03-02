c42488d12362ce4cc461dc342217d95d2b80fff3
Martin Zheng <jszhengyq@gmail.com>
2021-03-02 15:50:43 -0800

    Added indexed query method

---
+++ src/crud/get-indexed-query.ts
@@ -0,0 +1,19 @@
+import { getIndexKeyGen, hashByDefault } from '../indexing/get-index-key-gen';
+import { IndexedTable } from '../indexing/types';
+import { Row } from '../types';
+import { QueryParam } from './types';
+
+export function getIndexedQuery<TRow extends Row>(
+	sourceTable: IndexedTable<TRow>,
+	hash = hashByDefault,
+) {
+	return (query: QueryParam, isRowKeyOnly = false) => {
+		const fields = Object.keys(query);
+		const indexKey = getIndexKeyGen<QueryParam>(fields, hash)(query);
+
+		const rowKeys = sourceTable.index[indexKey];
+		return isRowKeyOnly
+			? rowKeys
+			: rowKeys.map((rowKey) => sourceTable.rows[rowKey]);
+	};
+}
