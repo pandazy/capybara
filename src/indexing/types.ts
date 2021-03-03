0b15b3786afefe921cd2470ec2c6b8088388c0f9
Martin Zheng <jszhengyq@gmail.com>
2021-03-02 16:20:34 -0800

    Removed unused type definitions

--- src/indexing/types.ts
+++ src/indexing/types.ts
@@ -1,9 +1,5 @@
 import { BasicTable, CommonMap, Row } from '../types';
 
-export type PrimaryIndex = { [key: string]: string };
-
-export type SecondaryIndex = { [key: string]: string[] };
-
 export type Index = {
 	[key: string]: string[];
 };
@@ -18,28 +14,3 @@ export interface Indexed {
 export interface IndexedTable<TRow extends Row>
 	extends BasicTable<TRow>,
 		Indexed {}
-
-export interface PrimaryIndexed {
-	primaryIndex: PrimaryIndex;
-	primaryIndexFields: string[];
-}
-
-export interface PrimaryIndexedTable<TRow extends Row>
-	extends BasicTable<TRow>,
-		PrimaryIndexed {}
-
-export interface SecondaryIndexed {
-	secondaryIndices: SecondaryIndex;
-	secondaryIndexFields: string[][];
-}
-
-export interface FullyIndexed extends PrimaryIndexed, SecondaryIndexed {}
-
-export interface SecondaryIndexedTable<TRow extends Row>
-	extends BasicTable<TRow>,
-		SecondaryIndexed {}
-
-export interface FullyIndexedTable<TRow extends Row>
-	extends BasicTable<TRow>,
-		PrimaryIndexed,
-		SecondaryIndexed {}
